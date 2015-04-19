ce2e657cb65bb5b143f8f61c3ad3efdb6e6570df
jaimani11 <jaimani1@gmail.com>
2015-04-19 17:05:36 +0000

    clone repo and modify readme

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,44 +9,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     
-    if @article.save
-      redirect_to @article
+    if @article.saved
+      redirect_to @article_params
     else
       render 'new'
     end
   end
-  
-  def edit
-    @article = Article.find(params[:id])
-  end
-  
-  def update
-    @article = Article.find(params[:id])
-    
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render 'edit'
-    end
-  end
-  
-  def show
-    @article = Article.find(params[:id])
-  end
-  
-  def index
-    @articles = Article.all
-  end
-  
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    
-    redirect_to articles_path
-  end
-  
-  private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
 end
+
